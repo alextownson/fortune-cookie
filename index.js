5fe6cd10a958cb24eb6854f6cf7791b5fccd797f
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-04-06 11:41:38 -0400

    Added animation and fortunes.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {fortuneData} from './fortuneData.js'
+
 const main = document.querySelector("main");
 
 window.addEventListener('load', () => {
@@ -11,3 +13,16 @@ window.addEventListener('resize', () => {
 addEventListener('deviceorientation', () => {
     main.style.height = window.innerHeight + 'px';
 });
+
+const openCookie = gsap.timeline();
+openCookie
+    .to("#fortune", {duration: 0.5, width: 100}, "1")
+    .to("#leftCookie", {duration: 0.5, rotation: 15, x: -30}, "1")
+    .to("#rightCookie", {duration: 0.5, rotation: -15, x: 30, y: -5}, "<")
+    .to("#fortune", {duration: 1.5, width: 420}, "2")
+    .to("#leftCookie", {duration: 1.5, x: -200}, "<")
+    .to("#rightCookie", {duration: 1.5, x: 218}, "<")
+
+let rand = Math.floor(Math.random() * 82)
+
+document.querySelector('#fortune').innerHTML = fortuneData[rand];
